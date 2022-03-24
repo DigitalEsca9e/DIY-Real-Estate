5d36618f42367fa914d63bad2ab5588f0070c8ad
br15cr <46728628+br15cr@users.noreply.github.com>
2022-03-24 11:59:37 -0400

    updated sign up page

--- scripts/signup-page-script.js
+++ scripts/signup-page-script.js
@@ -11,6 +11,8 @@ function start(){
     screens[0].style.display = "block";
 
     document.getElementById("signup-prev").disabled = true;
+
+    
 }
 
 function prev(){
@@ -27,19 +29,23 @@ function prev(){
 
 function next(){
     if(steps == 0){
-	var screens = document.getElementById("signup-screens").children;
+	if(checkValidFields()){
+	    window.location.href = "../pages/main-LoggedIn.html";
+	    var screens = document.getElementById("signup-screens").children;
 
-	screens[steps].style.display = "none";
-	screens[steps+1].style.display = "block";
-	steps = 1;
+	    screens[steps].style.display = "none";
+	    screens[steps+1].style.display = "block";
+	    steps = 1;
 
-	document.getElementById("signup-prev").disabled = false;
+	    document.getElementById("signup-prev").disabled = false;
+	}
     }
 }
 
 function togglePassword(){
     var pass = document.getElementById("password");
     var cPass = document.getElementById("pass-confirm");
+    var visButton = document.getElementById("show-pass-button");
     
     if(!showPassword){
 	showPassword = true;
@@ -47,13 +53,82 @@ function togglePassword(){
 
 	pass.type = "text";
 	cPass.type = "text";
+	visButton.classList.add("pass-vis-button-hide");
     }else{
 	showPassword = false;
 	pass.type = "password";
 	cPass.type = "password";
+	visButton.classList.remove("pass-vis-button-hide");
     }
 }
 
-function checkFirstPage(){
+function CheckEmailField(){
+    var emailstr = document.getElementById("email");
+    var split = emailstr.value.match(/\^([0-9a-fA-F]+).*\$/);
+    alert(split);
+    for(var i = 0; i < split.length;i++){
+	alert(i+". "+split[i]);
+    }
+}
+
+function clearError(element){
+    var error = document.getElementById("signup-error");
+    element.setCustomValidity("");
+    error.style.display = "none";
+    element.onchange = null;
+}
+
+function checkValidFields(){
+    var error = document.getElementById("signup-error");
     
+    var fName = document.getElementById("first-name");
+    var lName = document.getElementById("last-name");
+    var email = document.getElementById("email");
+
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    var cPass = document.getElementById("pass-confirm");
+    
+    if(fName.value.length == 0){
+	fName.setCustomValidity("invalid");
+	error.style.display = "block";
+	error.innerHTML = "Please Enter your first name.";
+	// fName.addListener("change",clearError(fName));
+	fName.onchange = function() {
+	    clearError(fName);
+	};
+	return false;
+    }
+
+    if(lName.value.length == 0){
+	lName.setCustomValidity('invalid');
+	error.style.display = 'block';
+	error.innerHTML = 'Please Enter your last name.';
+	lName.onchange = function(){
+	    clearError(lName);
+	};
+	return false;
+    }
+
+    if(email.value.length == 0){
+	email.setCustomValidity('invalid');
+	error.style.display = 'block';
+	error.innerHTML = 'Please Enter your email address.';
+	email.onchange = function(){
+	    clearError(email);
+	};
+	return false;
+    }
+
+    if(username.value.length == 0){
+	username.setCustomValidity('invalid');
+	error.style.display = 'block';
+	error.innerHTML = 'Please Enter your username.';
+	username.onchange = function(){
+	    clearError(username);
+	};
+	return false;
+    }
+
+    return true;
 }
